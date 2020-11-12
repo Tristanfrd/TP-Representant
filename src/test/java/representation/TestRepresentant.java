5ba32e9072caaca137942fe8d085077682f92bcb
Tristanfrd <Tristanfrd@users.noreply.github.com>
2020-11-12 13:07:00 +0100

    Commit

--- src/test/java/representation/TestRepresentant.java
+++ src/test/java/representation/TestRepresentant.java
@@ -25,16 +25,14 @@ public class TestRepresentant {
 	
 	@Test
 	public void testSalaireMensuel() {
+            r.CAParDefaut(0);
 		float CA = 50000f;
 		float POURCENTAGE= 0.1f; // 10% de pourcentage sur CA
 		// On enregistre un CA pour le mois 0 (janvier)
-		r.enregistrerCA(0, CA);
-		
+		r.enregistrerCA(0, CA);		
 		// On calcule son salaire pour le mois 0 avec 10% de part sur CA
 		float salaire = r.salaireMensuel(0, POURCENTAGE);
-		
 		// A quel résultat on s'attend ?
-		
 		assertEquals(// Comparaison de "float"
 			// valeur attendue
 			FIXE_BASTIDE + INDEMNITE_OCCITANIE + CA * POURCENTAGE,
@@ -53,24 +51,18 @@ public class TestRepresentant {
 		
 		// On n'enregistre aucun CA
 		//r.enregistrerCA(0, 10000f);
-		
+		r.CAParDefaut(0);
 		// On calcule son salaire pour le mois 0 avec 10% de part sur CA
 		float salaire = r.salaireMensuel(0, POURCENTAGE);
 		
 		// A quel résultat on s'attend ?
 		// Le CA du mois doit avoir été initialisé à 0
 		
-		assertEquals(
-			FIXE_BASTIDE + INDEMNITE_OCCITANIE, 
-			salaire, 
-			0.001,
-			"Le CA n'est pas correctement initialisé"
-		);
+		assertEquals(FIXE_BASTIDE + INDEMNITE_OCCITANIE,salaire,0.001,"Le CA n'est pas correctement initialisé");
 	}
 
 	@Test
 	public void testCANegatifImpossible() {
-		
 		try {
 			// On enregistre un CA négatif, que doit-il se passer ?
 			// On s'attend à recevoir une exception
@@ -80,8 +72,37 @@ public class TestRepresentant {
 		} catch (IllegalArgumentException e) {
 			// Si on arrive ici, c'est normal, c'est le comportement attendu
 		}
-
 	}
+        
+        @Test
+	public void testCAMoisIncorrect(){
+            try{
+                r.enregistrerCA(-1, FIXE_BASTIDE);
+                fail("Un mois incorrect doit générer une exception");
+            }catch(IllegalArgumentException e){
+                
+            }
+        }
+        
+        @Test
+	public void testSalaireMoisIncorrect(){
+            try{
+                r.salaireMensuel(-1, 0.1f);
+                fail("Un mois incorrect doit générer une exception");                
+            } catch(IllegalArgumentException e){
+                
+            }
+        }
+        
+        @Test
+	public void testSalairePourcentageNegatif(){
+            try{
+               r.salaireMensuel(0, -0.1f);
+               fail("Un pourcentage négatif doit renvoyer une exception.");
+            }catch(IllegalArgumentException e){
+                
+            }
+        }
 	
 	
 }
